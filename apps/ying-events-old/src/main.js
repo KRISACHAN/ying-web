ba2a9007e6d0e7b330765e0f51e3c50b0157bc0b
fish_head <chenjinwen77@foxmail.com>
2024-11-16 01:24:22 +0800

    feat: removed ying-events-old

--- apps/ying-events-old/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import './styles/index.less'
-import App from './App.vue'
-
-import router from './router'
-import store from './store'
-
-const app = createApp(App)
-app.use(router).use(store).mount('#app')
--- apps/ying-events-old/src/store/index.js
+++
@@ -1,5 +0,0 @@
-import { createPinia } from 'pinia'
-import useLotteryStore from './modules/lottery'
-const store = createPinia()
-export { useLotteryStore }
-export default store
