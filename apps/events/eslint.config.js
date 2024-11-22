5662ffcaf5e2ab398fe6c2b492b8995a070a20c2
fish_head <chenjinwen77@foxmail.com>
2024-11-22 14:17:20 +0800

    bugfix: fix the bug of IDE can not recognize @ path

--- apps/events/eslint.config.js
+++ apps/events/eslint.config.js
@@ -5,45 +5,47 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-plugin-prettier';
 
-export default tseslint.config(
-    { ignores: ['dist', 'node_modules'] },
-    {
-        files: ['**/*.{js,jsx,ts,tsx}'],
-        extends: [js.configs.recommended, ...tseslint.configs.recommended],
-        languageOptions: {
-            ecmaVersion: 2020,
-            globals: {
-                ...globals.browser,
-                React: 'readonly',
-            },
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true,
-                },
-            },
+export default tseslint.config({
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['dist', 'node_modules'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    languageOptions: {
+        ecmaVersion: 2020,
+        globals: {
+            ...globals.browser,
+            React: 'readonly',
         },
-        plugins: {
-            'react-hooks': reactHooks,
-            'react-refresh': reactRefresh,
-            prettier: prettier,
-        },
-        rules: {
-            ...reactHooks.configs.recommended.rules,
-            'react-refresh/only-export-components': [
-                'warn',
-                { allowConstantExport: true },
-            ],
-            indent: [0, 4],
-            'arrow-parens': 0,
-            'generator-star-spacing': 0,
-            'no-debugger': 0,
-            'eol-last': 0,
-            eqeqeq: 2,
-            camelcase: 0,
-            'space-before-function-paren': 0,
-            quotes: ['error', 'single'],
-            'prettier/prettier': 'error',
-            'no-var': 'error',
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
         },
     },
-);
+    plugins: {
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
+        prettier: prettier,
+    },
+    rules: {
+        ...reactHooks.configs.recommended.rules,
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+        indent: [0, 4],
+        'arrow-parens': 0,
+        'generator-star-spacing': 0,
+        'no-debugger': 0,
+        'eol-last': 0,
+        eqeqeq: 2,
+        camelcase: 0,
+        'space-before-function-paren': 0,
+        quotes: ['error', 'single'],
+        'prettier/prettier': 'error',
+        'no-var': 'error',
+    },
+    env: {
+        browser: true,
+        node: true,
+    },
+});
