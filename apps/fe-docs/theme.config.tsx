7cd6a50afb6b43bbc65fd77796d758f22ef291cc
fish_head <chenjinwen77@foxmail.com>
2024-12-02 16:19:06 +0800

    chore(fe-docs): update project structure and add favicon

--- apps/fe-docs/theme.config.tsx
+++ apps/fe-docs/theme.config.tsx
@@ -1,12 +1,15 @@
+import Image from 'next/image';
 import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-    logo: <span>@ying-web</span>,
+    logo: (
+        <Image src="/images/icon.png" alt="@ying-web" width={24} height={24} />
+    ),
     project: {
         link: 'https://github.com/KRISACHAN/ying-web',
     },
     docsRepositoryBase:
-        'https://github.com/KRISACHAN/ying-web/tree/main/apps/docs',
+        'https://github.com/KRISACHAN/ying-web/tree/prod/apps/fe-docs',
     useNextSeoProps() {
         return {
             titleTemplate: '%s – @ying-web',
