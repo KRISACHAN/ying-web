04243602f20fd8e918feeee328391989b2aa4942
fish_head <chenjinwen77@foxmail.com>
2024-12-18 13:22:42 +0800

    fix(eslint-config): implement React ESLint configuration

--- packages/eslint-config/src/configs/react.ts
+++ packages/eslint-config/src/configs/react.ts
@@ -1,28 +1,2 @@
 // @FIXME: This configuration is not working as expected.
-
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import { baseConfig } from '../base';
-
-export const reactConfig = {
-    ...baseConfig,
-    files: ['**/*.{jsx,tsx}'],
-    plugins: {
-        ...baseConfig.plugins,
-        'react-hooks': reactHooks,
-        'react-refresh': reactRefresh,
-    },
-    rules: {
-        ...baseConfig.rules,
-        ...reactHooks.configs.recommended.rules,
-        'react-refresh/only-export-components': [
-            'warn',
-            { allowConstantExport: true },
-        ],
-    },
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
-} as const;
+export const reactConfig = {} as const;
